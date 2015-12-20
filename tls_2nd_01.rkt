9ffccc91acf8aea3e1ddaba234e1825406f0d54e
chpwang <chpengwang@gmail.com>
2015-12-20 19:09:06 +0800

    add codes of Chapter 4

--- tls_2nd_01.rkt
+++ tls_2nd_01.rkt
@@ -115,6 +115,107 @@
   (lambda (n)
     (+ n 1)))
 
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define addtup
+  (lambda (tup)
+    (cond ((null? tup) 0)
+          (else
+           (+ (car tup)
+              (addtup (cdr tup)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond ((null? tup1) tup2)
+          ((null? tup2) tup1)
+          (else
+           (cons (+ (car tup1) (car tup2))
+                 (tup+ (cdr tup1)
+                       (cdr tup2)))))))
+
+(define gthan
+  (lambda (n m)
+    (cond ((zero? n) #f)
+          ((zero? m) #t)
+          (else
+           (gthan (sub1 n) (sub1 m))))))
+
+(define lthan
+  (lambda (n m)
+    (cond ((zero? m) #f)
+          ((zero? n) #t)
+          (else
+           (lthan (sub1 n) (sub1 m))))))
+
+(define eqto
+  (lambda (n m)
+    (cond ((gthan n m) #f)
+          ((lthan n m) #f)
+          (else
+           #t))))
+
+(define expo
+  (lambda (n m)
+    (cond ((zero? m) 1)
+          (else
+           (* n (expo n (sub1 m)))))))
+
+(define length
+  (lambda (lat)
+    (cond ((null? lat) 0)
+          (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond ((zero? (sub1 n)) (car lat))
+          (else (pick (sub1 n)
+                      (cdr lat))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond ((null? lat) '())
+          ((number? (car lat))
+           (no-nums (cdr lat)))
+          (else (cons (car lat)
+                      (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond ((null? lat) '())
+          ((number? (car lat))
+           (cons (car lat)
+                 (all-nums (cdr lat))))
+          (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond ((and (number? a1) (number? a2))
+           (= a1 a2))
+          ((or (number? a1) (number? a2))
+           #f)
+          (else (eq? a1 a2)))))
+
+(define occur
+  (lambda (a lat)
+    (cond ((null? lat) 0)
+          ((eqan? a (car lat))
+           (add1 (occur a (cdr lat))))
+          (else
+           (occur a (cdr lat))))))
+
+(define one?
+  (lambda (n)
+    (= n 1)))
+
+(define rempick
+  (lambda (n lat)
+    (cond ((one? n) (cdr lat))
+          (else (cons (car lat)
+                      (rempick (sub1 n)
+                               (cdr lat)))))))
+
 
 
 

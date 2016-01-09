3b2de774f5a241917b9386ea058ce9fc53d23207
chpwang <chpengwang@gmail.com>
2016-01-09 14:08:59 +0800

    add codes of Chapter 12 - Take Cover

--- tss_2nd_01.rkt
+++ tss_2nd_01.rkt
@@ -1,12 +1,5 @@
 #lang scheme
 
-(define member?
-  (lambda (a lat)
-    (cond ((null? lat) #f)
-          (else
-           (or (eq? a (car lat))
-               (member? a (cdr lat)))))))
-
 #|
 我的版本，包含了不相邻（连续）的判断，但书里要求要相邻才 #t
 
@@ -24,38 +17,6 @@
           (else
            (eq? a (car lat))))))
 
-(define two-in-a-row-b?
-  (lambda (a lat)
-    (cond ((null? lat) #f)
-          (else
-           (or (eq? a (car lat))
-               (two-in-a-row-b? (car lat) (cdr lat)))))))
-
-(define two-in-a-row?
-  (lambda (lat)
-    (cond ((null? lat) #f)
-          (else
-           (two-in-a-row-b? (car lat) (cdr lat))))))
-
-;(two-in-a-row? '(Italian sardines spaghetti sardines parsley))
-
-(define sum-of-prefixes-help
-  (lambda (prefixes tup)
-    (cond ((null? tup) '())
-          (else
-           (cons (+ prefixes (car tup))
-                 (sum-of-prefixes-help (+ prefixes (car tup))
-                                       (cdr tup)))))))
-
-(define sum-of-prefixes
-  (lambda (tup)
-    (cond ((null? tup) '())
-          (else
-           (sum-of-prefixes-help 0 tup)))))
-
-
-
-;(sum-of-prefixes '(2 1 9 17 0))
 
 #|
 ;;;我写出 sum-of-prefixes 后突然想到然后写出的 SICP 的 2.18
@@ -77,23 +38,86 @@
 (reverse-item '(2 1 9 17 0))
 |#
 
-(define scramble
-  (lambda (tup)
-    (cond ((null? tup) '())
-          (else
-           (scramble-help '()
-                          tup)))))
+;;;;;;; Chapter 2 - Take Cover ;;;;;;;
 
-(define scramble-help
-  (lambda (prefixes tup)
-    (cond ((null? tup) '())
-          (else
-           (cons (pick (car tup)
-                           (cons (car tup)
-                                 prefixes))
-                 (scramble-help (cons (car tup)
-                                      prefixes)
-                                (cdr tup)))))))
+(define multirember
+  (letrec ((multirember (lambda (a lat)
+                          (cond ((null? lat) '())
+                                ((eq? a (car lat))
+                                 (multirember a (cdr lat)))
+                                (else
+                                 (cons (car lat)
+                                       (multirember a (cdr lat))))))))
+    multirember))
+
+(define multirember-f
+  (lambda (test?)
+    (letrec ((m-f (lambda (a lat)
+                    (cond ((null? lat) '())
+                          ((test? a (car lat))
+                           (m-f a (cdr lat)))
+                          (else (cons (car lat)
+                                      (m-f a (cdr lat))))))))
+      m-f)))
+
+;(multirember 'pie '(apple custard pie linzer pie torte))
+
+(define member?
+  (lambda (a lat)
+    (letrec ((m (lambda (l)
+                  (cond ((null? l) #f)
+                        (else
+                         (or (eq? a (car l))
+                             (m (cdr l))))))))
+      (m lat))))
+
+;(member? 'ice '(salad greens with pears ice brie chesse frozen yogurt))
+
+(define union
+  (lambda (set1 set2)
+    (letrec ((u (lambda (s)
+                  (cond ((null? s) set2)
+                        ((M? (car s) set2)
+                         (u (cdr s)))
+                        (else
+                         (cons (car s)
+                               (u (cdr s)))))))
+             (M? (lambda (a lat)
+                   (letrec ((m (lambda (l)
+                                 (cond ((null? l) #f)
+                                       (else
+                                        (or (eq? a (car l))
+                                            (m (cdr l))))))))
+                     (m lat)))))
+      (u set1))))
+
+;(union '(tomatoes and macaroni tt) '(macaroni and cheese))
+
+(define two-in-a-row?
+  (letrec ((help (lambda (a lat)
+                   (cond ((null? lat) #f)
+                         (else
+                          (or (eq? a (car lat))
+                              (help (car lat) (cdr lat))))))))
+    (lambda (lat)
+      (cond ((null? lat) #f)
+            (else
+             (help (car lat) (cdr lat)))))))
+
+;(two-in-a-row? '(Italian sardines spaghetti sardines parsley))
+
+
+(define sum-of-prefixes
+  (letrec ((help (lambda (prefixes tup)
+                   (cond ((null? tup) '())
+                         (else
+                          (cons (+ prefixes (car tup))
+                                (help (+ prefixes (car tup))
+                                      (cdr tup))))))))
+    (lambda (tup)
+      (help 0 tup))))
+
+;(sum-of-prefixes '(2 1 9 17 0))
 
 (define pick
   (lambda (n l)
@@ -101,7 +125,28 @@
           (else
            (pick (- n 1) (cdr l))))))
 
+(define scramble
+  (lambda (tup)
+    (letrec ((S (lambda (prefixes tup)
+                  (cond ((null? tup) '())
+                        (else
+                         (cons (pick (car tup)
+                                  (cons (car tup)
+                                        prefixes))
+                               (S (cons (car tup)
+                                        prefixes)
+                                  (cdr tup))))))))
+      (S '() tup))))
+
 (scramble '(1 2 3 1 2 3 4 1 8 2 10))
+
+
+;;;;;;; Chapter 13 - Hop, Skip, and Jump ;;;;;;;
+
+
+
+
+
 
 
 

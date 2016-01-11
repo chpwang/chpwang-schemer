82a795af46d260247aa13302e1044491684395a9
chpwang <chpengwang@gmail.com>
2016-01-11 19:00:03 +0800

    add codes of Chapter 14 - Let There Be Names

--- tss_2nd_01.rkt
+++ tss_2nd_01.rkt
@@ -11,6 +11,41 @@
      (letcc success 
        (letcc var (success a)) . b))))
 
+(define length
+  (lambda (l)
+    (cond ((null? l) 0)
+          (else
+           (+ 1 (length (cdr l)))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond ((and (number? a1) (number? a2))
+           (= a1 a2))
+          ((or (number? a1) (number? a2))
+           #f)
+          (else (eq? a1 a2)))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond ((and (null? l1) (null? l2))
+           #t)
+          ((or (null? l1) (null? l2))
+           #f)
+          (else
+           (and (equal? (car l1) (car l2))
+                (eqlist? (cdr l1) (cdr l2)))))))
+
+(define equal?
+  (lambda (s1 s2)
+    (cond ((and (atom? s1) (atom? s2))
+           (eqan? s1 s2))
+          ((or (atom? s1) (atom? s2))
+           #f)
+          (else
+           (eqlist? s1 s2)))))
+
+
+
 #|
 我的版本，包含了不相邻（连续）的判断，但书里要求要相邻才 #t
 
@@ -136,20 +171,6 @@
           (else
            (pick (- n 1) (cdr l))))))
 
-(define scramble
-  (lambda (tup)
-    (letrec ((S (lambda (prefixes tup)
-                  (cond ((null? tup) '())
-                        (else
-                         (cons (pick (car tup)
-                                  (cons (car tup)
-                                        prefixes))
-                               (S (cons (car tup)
-                                        prefixes)
-                                  (cdr tup))))))))
-      (S '() tup))))
-
-;(scramble '(1 2 3 1 2 3 4 1 8 2 10))
 
 
 ;;;;;;; Chapter 13 - Hop, Skip, and Jump ;;;;;;;
@@ -234,6 +255,204 @@
 ;(rember-upto-last 'roots '(noodles spaghetti roots spatzle bean-thread roots potatoes yam others rice))
 
 ;;;;;;; Chapter 14 - Let There Be Names ;;;;;;;
+(define atom?
+  (lambda (s)
+    (and (not (pair? s)) (not (null? s)))))
+
+
+#|
+;;;;;; 我写的复杂版本 - 对比书上的第一版本好像还可以
+(define rember1*
+  (lambda (a l)
+    (letrec ((R1* (lambda (ls)
+                    (cond ((null? ls) '())
+                          ((atom? (car ls))
+                           (cond ((eq? (car ls) a)
+                                  (cdr ls))
+                                 (else
+                                  (cons (car ls)
+                                        (R1* (cdr ls))))))
+                          (else
+                           (cond ((= (L (car ls)) (L (R1* (car ls))))
+                                  (cons (car ls)
+                                        (R1* (cdr ls))))
+                                 (else
+                                  (cons (R1* (car ls))
+                                        (cdr ls))))))))
+             (L (lambda (s)
+                  (cond ((null? s) 0)
+                        ((atom? (car s))
+                         (+ 1 (L (cdr s))))
+                        (else
+                         (+ (L (car s)) (L (cdr s))))))))
+      (R1* l))))
+|#
+
+(define max
+  (lambda (a b)
+    (if (> a b) a b)))
+
+(define depth*
+  (lambda (l)
+    (cond ((null? l) 1)
+          ((atom? (car l))
+           (depth* (cdr l)))
+          (else
+           (max
+            (+ 1 (depth* (car l)))
+            (depth* (cdr l)))))))
+
+;(depth* '(() ((bitter butter) (makes) (batter (bitter))) butter))
+
+(define scramble
+  (lambda (tup)
+    (letrec ((S (lambda (prefixes tup)
+                  (cond ((null? tup) '())
+                        (else
+                         (let ((rp (cons (car tup)
+                                         prefixes)))
+                           (cons (pick (car tup)
+                                       rp)
+                                 (S rp
+                                    (cdr tup)))))))))
+      (S '() tup))))
+
+;(scramble '(1 2 3 1 2 3 4 1 8 2 10))
+
+#|
+;;;;; old version of leftmost
+(define leftmost
+  (lambda (l)
+    (cond ((null? l) '())
+          ((atom? (car l))
+           (car l))
+          (else
+           (let ((carl (leftmost (car l))))
+             (cond ((atom? carl) carl)
+                   (else
+                    (leftmost (cdr l)))))))))
+|#
+
+#|
+;;;;;; 我自己写的新版本
+(define leftmost
+  (lambda (l)
+    (letcc hop
+      (letrec ((le (lambda (l)
+                     (cond ((null? l) '())
+                           ((atom? (car l))
+                            (hop (car l)))
+                           (else
+                            (le (car l))
+                            (le (cdr l)))))))
+        (le l)))))
+|#
+
+(define leftmost
+  (lambda (l)
+    (letcc hop
+      (letrec ((le (lambda (l)
+                     (cond ((null? l) '())
+                           ((atom? (car l))
+                            (hop (car l)))
+                           (else
+                            (le (car l))
+                            (le (cdr l)))))))
+        (le l)))))
+
+;(leftmost '((() ((())) (() (ddc av))) b (c)))
+
+#|
+;;;;;; 老版本
+(define rember1*
+  (lambda (a l)
+    (letrec ((R1* (lambda (ls)
+                    (cond ((null? ls) '())
+                          ((atom? (car ls))
+                           (cond ((eq? (car ls) a)
+                                  (cdr ls))
+                                 (else
+                                  (cons (car ls)
+                                        (R1* (cdr ls))))))
+                          (else
+                           (let ((rcar (R1* (car ls))))
+                             (cond ((eqlist? (car ls) rcar)
+                                    (cons (car ls)
+                                          (R1* (cdr ls))))
+                                        (else
+                                         (cons rcar
+                                               (cdr ls))))))))))
+      (R1* l))))
+|#
+
+#|
+;;;;;;; 我写的版本 - 此解关键是洞察并构造出成功移除与否的返回值，成功为 list，失败为 atom
+;;;;;;; 看了书上的版本后，反思觉得不要过早使用 letrec, let 等，先写出函数再化简
+(define rember1*
+  (lambda (a l)
+    (letrec ((R1* (lambda (ls out)
+                    (cond ((null? ls) (out #f))
+                          ((atom? (car ls))
+                           (cond ((eq? (car ls) a)
+                                  (cdr ls))
+                                 (else
+                                  (cons (car ls)
+                                        (R1* (cdr ls) out)))))
+                          (else
+                           (let ((cl (help (car ls))))
+                             (if (atom? cl)
+                                 (cons (car ls)
+                                       (R1* (cdr ls) out))
+                                 (cons cl
+                                       (cdr ls))))))))
+             (help (lambda (li)
+                     (letcc hop
+                       (R1* li hop)))))
+      (help l))))
+
+;(rember1* 'tomatoes '((pasta meat) pasta (noodles meat sauce) meat tomatoes))
+;(rember1* 'salad '((Swedish rye) (French (mustard ssalad turkey)) (salad)))
+;(rember1* 'salad '(((a)) b b))
+|#
+
+(define rm
+  (lambda (a ls oh)
+    (cond ((null? ls) (oh #f))
+          ((atom? (car ls))
+           (cond ((eq? (car ls) a)
+                  (cdr ls))
+                 (else
+                  (cons (car ls)
+                        (rm a (cdr ls) oh)))))
+          (else
+           (if (atom? (letcc out
+                        (rm a (car ls) out)))
+               (cons (car ls)
+                     (rm a (cdr ls) oh))
+               (cons (rm a (car ls) oh)
+                     (cdr ls)))))))
+
+(define rember1*
+  (lambda (a l)
+    (let ((new-l (letcc hop
+                   (rm a l hop))))
+      (if (atom? new-l)
+          l
+          new-l))))
+
+(define rember1**
+  (lambda (a l)
+    (try hop (rm a l hop) l)))
+
+(rember1** 'meat '((pasta meat) pasta (noodles (meat) sauce) meat tomatoes))
+(rember1** 'salad '((Swedish rye) (French (mustard salad turkey)) (salad)))
+
+
+
+;;;;;;; Chapter 15 - The Difference Between Men and Boys ... ;;;;;;;
+
+
+
 
 
 
